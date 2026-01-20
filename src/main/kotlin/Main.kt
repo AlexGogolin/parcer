3355c2b3d1642111512b5fca86f64327d58ba32a
NukeKB <alexey-seo@clientprosto.ru>
2026-01-20 09:26:58 +0300

    Мелкие исправления

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -4,10 +4,8 @@ import org.jsoup.Jsoup
 import org.jsoup.nodes.Document
 import org.jsoup.nodes.Element
 
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 fun main() {
-    System.setOut(java.io.PrintStream(System.`out`, true, "UTF-8"))
+    System.setOut(java.io.PrintStream(System.`out`, true, "UTF-8")) //Для кирилицы в консоли, иначе не читается
 
     val doc: Document = Jsoup.connect("https://mybook.ru/author/duglas-adams/avtostopom-po-galaktike-restoran-u-konca-vselennoj/citations/").get()
     val result = doc.select(".sc-2aegk7-2")
