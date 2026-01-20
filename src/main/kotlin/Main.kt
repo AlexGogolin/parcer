58a9ce1050c440b0361300358cb748689944e8bc
NukeKB <alexey-seo@clientprosto.ru>
2026-01-20 09:30:21 +0300

    Создание отдельной ветки для пул реквеста

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,16 +1,6 @@
 package org.example
 
-import org.jsoup.Jsoup
-import org.jsoup.nodes.Document
-import org.jsoup.nodes.Element
 
 fun main() {
-    System.setOut(java.io.PrintStream(System.`out`, true, "UTF-8")) //Для кирилицы в консоли, иначе не читается
 
-    val doc: Document = Jsoup.connect("https://mybook.ru/author/duglas-adams/avtostopom-po-galaktike-restoran-u-konca-vselennoj/citations/").get()
-    val result = doc.select(".sc-2aegk7-2")
-
-    for(bloquote: Element in result) {
-        println("${bloquote.text()}\n")
-    }
 }
